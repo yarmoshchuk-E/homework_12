47acc2803ed54a173e6668a0afbc92da35cf889e
yarmoshchuk-E <yarmoshchuk_80@mail.ru>
2026-01-14 12:28:17 +0400

    выполнил ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task_1");
+        String firstName = "Ivan ";
+        String middleName = "Ivanovich ";
+        String lastName = "Ivanov ";
+        String fullName = lastName + firstName + middleName;
+        System.out.println("Ф.И.О. сотрудника - " + fullName);
+        System.out.println();
 
+        System.out.println("Task_2");
+        String dataReport = fullName.toUpperCase();   // строка для заполнения отчёта
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + dataReport);
+        System.out.println();
+
+        System.out.println("Task_3");
+        fullName = "Иванов Семён Семёнович";
+        String fullName3 = fullName.replace("ё", "е");
+        System.out.println("Данные Ф.И.О. сотрудника - " + fullName3);
     }
 }
